12fd695de9bdffc2633cf4ef67923251395146da
evanszymkowicz <evan.szymkowicz@evanwolf.co>
2019-02-17 17:37:12 -0500

    theme

---
+++ src/theme/variables.js
@@ -0,0 +1,27 @@
+export const headlineFontFamily = 'Josefin Sans';
+export const normalFontFamily = `'Josefin Sans', sans-serif`;
+export const normalFontSize = '24px';
+
+export const color1 = '#031D44'; // Oxford blue
+export const color2 = '#04395E'; // Dark Blue
+export const color3 = '#70A288'; // Light Green
+export const color4 = 'white';
+export const color5 = '#D5896F'; // Light Yellow
+
+export const black = '#424242'; // Charcoal
+export const white = 'white';
+export const red = 'rgba(242, 95, 92, 1)'; // Dark Orange
+export const yellow = 'rgb(253, 238, 108)'; // Light Yellow
+export const blue = 'rgba(36, 123, 160, 1)'; // Blueish
+export const gray = '#828282';
+
+export const menuBackground = color1;
+export const menuTextColor = color5;
+
+export const backgroundColor = color5;
+export const textColor = color1;
+
+export const socialMediaColor = color2;
+export const socialMediaInverseColor = color4;
+
+export const arrowColor = color3;
